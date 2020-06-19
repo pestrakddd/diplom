5f90c5273d8e76e316ac6e18bf4433131814e6c4
pestrakDDD <57468197+pestrakDDD@users.noreply.github.com>
2020-06-19 13:59:36 +0300

    first424234

--- src/main/java/Poi/Poi_read.java
+++ src/main/java/Poi/Poi_read.java
@@ -52,168 +52,173 @@ public class Poi_read {
     }
 //String pathCreate, ArrayList<Stipendia_zaMesaz> listCreate
     @SuppressWarnings("deprecation")
-    public void writeIntoExcelStipendiaYear(String path, ArrayList <Stipendia_zaMesaz> stipendia_zaMesaz, String year) throws FileNotFoundException, IOException{
+    public boolean writeIntoExcelStipendiaYear(String path, ArrayList <Stipendia_zaMesaz> stipendia_zaMesaz, String year) throws FileNotFoundException, IOException{
 
-        HSSFWorkbook book = new HSSFWorkbook();
-        HSSFSheet sheet = book.createSheet("Стипендия");
+        try {
+            HSSFWorkbook book = new HSSFWorkbook();
+            HSSFSheet sheet = book.createSheet("Стипендия");
 
-        HSSFFont font = book.createFont();
+            HSSFFont font = book.createFont();
 
-        HSSFCellStyle style = book.createCellStyle();
-        HSSFCellStyle style1 = book.createCellStyle();
-        HSSFCellStyle style2 = book.createCellStyle();
-        HSSFCellStyle style3 = book.createCellStyle();
+            HSSFCellStyle style = book.createCellStyle();
+            HSSFCellStyle style1 = book.createCellStyle();
+            HSSFCellStyle style2 = book.createCellStyle();
+            HSSFCellStyle style3 = book.createCellStyle();
 
 
-        style.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
-        style.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
-        style.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
-        style.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
-        style1.setAlignment(style.ALIGN_CENTER);
-        style1.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
-        style1.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
-        style1.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
-        style1.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
-        style1.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
-        style2.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
-        style2.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
-        style2.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
-        style2.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
-        style2.setAlignment(style.ALIGN_CENTER);
-        style2.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
-        style3.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
-        style3.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
+            style.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
+            style.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
+            style.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
+            style.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
+            style1.setAlignment(style.ALIGN_CENTER);
+            style1.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+            style1.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
+            style1.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
+            style1.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
+            style1.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
+            style2.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
+            style2.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
+            style2.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
+            style2.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
+            style2.setAlignment(style.ALIGN_CENTER);
+            style2.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+            style3.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
+            style3.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
 
-        // и применяем к этому стилю жирный шрифт
-        style.setFont(font);
-        font.setFontHeightInPoints((short) 12);
-        font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-        font.setFontName("Times New Roman");
-        style.setAlignment(style.ALIGN_CENTER);
+            // и применяем к этому стилю жирный шрифт
+            style.setFont(font);
+            font.setFontHeightInPoints((short) 12);
+            font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+            font.setFontName("Times New Roman");
+            style.setAlignment(style.ALIGN_CENTER);
 
-        HSSFRow row = sheet.createRow((short)0);
-        style1.setWrapText(true);
-        row.setRowStyle(style1);
+            HSSFRow row = sheet.createRow((short)0);
+            style1.setWrapText(true);
+            row.setRowStyle(style1);
 
-        sheet.addMergedRegion(new CellRangeAddress(0,0,0,4));
-        sheet.addMergedRegion(new CellRangeAddress(1,1,0,4));
-        sheet.autoSizeColumn(0);
+            sheet.addMergedRegion(new CellRangeAddress(0,0,0,4));
+            sheet.addMergedRegion(new CellRangeAddress(1,1,0,4));
+            sheet.autoSizeColumn(0);
 
-        Cell cell1 = row.createCell(0);
-        Cell cell1_1 = row.createCell(1);
-        Cell cell1_2 = row.createCell(2);
-        Cell cell1_3 = row.createCell(3);
-        Cell cell1_4 = row.createCell(4);
-        row.getCell(0).setCellStyle(style);
-        row.getCell(1).setCellStyle(style);
-        row.getCell(2).setCellStyle(style);
-        row.getCell(3).setCellStyle(style);
-        row.getCell(4).setCellStyle(style);
-        cell1.setCellValue("С Т И П Е Н Д И Я");
-        row = sheet.createRow(1);
-        Cell cell2 = row.createCell(0);
-        Cell cell2_1 = row.createCell(1);
-        Cell cell2_2 = row.createCell(2);
-        Cell cell2_3 = row.createCell(3);
-        Cell cell2_4 = row.createCell(4);
-        row.getCell(0).setCellStyle(style);
-        row.getCell(1).setCellStyle(style);
-        row.getCell(2).setCellStyle(style);
-        row.getCell(3).setCellStyle(style);
-        row.getCell(4).setCellStyle(style);
-        row.getCell(0).setCellStyle(style);
-        cell2.setCellValue(year + " год");
-        row = sheet.createRow(2);
-        Cell cell3_1 = row.createCell(0);
-        row.getCell(0).setCellStyle(style);
-        cell3_1.setCellValue("№ п/п");
-        sheet.autoSizeColumn(0);
-        Cell cell3_2 = row.createCell(1);
-        row.getCell(1).setCellStyle(style);
-        cell3_2.setCellValue("Тип");
-        sheet.autoSizeColumn(1);
-        Cell cell3_3 = row.createCell(2);
-        row.getCell(2).setCellStyle(style);
-        cell3_3.setCellValue("    Фамилия     Имя     Отчество    ");
-        sheet.autoSizeColumn(2);
-        Cell cell3_4 = row.createCell(3);
-        row.getCell(3).setCellStyle(style);
-        cell3_4.setCellValue("Сумма, руб");
-        sheet.autoSizeColumn(3);
-        Cell cell3_5 = row.createCell(4);
-        row.getCell(4).setCellStyle(style);
-        cell3_5.setCellValue("Примечание");
-        sheet.autoSizeColumn(4);
+            Cell cell1 = row.createCell(0);
+            Cell cell1_1 = row.createCell(1);
+            Cell cell1_2 = row.createCell(2);
+            Cell cell1_3 = row.createCell(3);
+            Cell cell1_4 = row.createCell(4);
+            row.getCell(0).setCellStyle(style);
+            row.getCell(1).setCellStyle(style);
+            row.getCell(2).setCellStyle(style);
+            row.getCell(3).setCellStyle(style);
+            row.getCell(4).setCellStyle(style);
+            cell1.setCellValue("С Т И П Е Н Д И Я");
+            row = sheet.createRow(1);
+            Cell cell2 = row.createCell(0);
+            Cell cell2_1 = row.createCell(1);
+            Cell cell2_2 = row.createCell(2);
+            Cell cell2_3 = row.createCell(3);
+            Cell cell2_4 = row.createCell(4);
+            row.getCell(0).setCellStyle(style);
+            row.getCell(1).setCellStyle(style);
+            row.getCell(2).setCellStyle(style);
+            row.getCell(3).setCellStyle(style);
+            row.getCell(4).setCellStyle(style);
+            row.getCell(0).setCellStyle(style);
+            cell2.setCellValue(year + " год");
+            row = sheet.createRow(2);
+            Cell cell3_1 = row.createCell(0);
+            row.getCell(0).setCellStyle(style);
+            cell3_1.setCellValue("№ п/п");
+            sheet.autoSizeColumn(0);
+            Cell cell3_2 = row.createCell(1);
+            row.getCell(1).setCellStyle(style);
+            cell3_2.setCellValue("Тип");
+            sheet.autoSizeColumn(1);
+            Cell cell3_3 = row.createCell(2);
+            row.getCell(2).setCellStyle(style);
+            cell3_3.setCellValue("    Фамилия     Имя     Отчество    ");
+            sheet.autoSizeColumn(2);
+            Cell cell3_4 = row.createCell(3);
+            row.getCell(3).setCellStyle(style);
+            cell3_4.setCellValue("Сумма, руб");
+            sheet.autoSizeColumn(3);
+            Cell cell3_5 = row.createCell(4);
+            row.getCell(4).setCellStyle(style);
+            cell3_5.setCellValue("Примечание");
+            sheet.autoSizeColumn(4);
 
-        double summ = 0;
-        int oD = 0;
-        int cE = 0;
-        int i;
-        for (i = 0; i < stipendia_zaMesaz.size(); i++) {
+            double summ = 0;
+            int oD = 0;
+            int cE = 0;
+            int i;
+            for (i = 0; i < stipendia_zaMesaz.size(); i++) {
+                row = sheet.createRow(i+3);
+                Cell cell4_1 = row.createCell(0);
+                row.getCell(0).setCellStyle(style2);
+                cell4_1.setCellValue(stipendia_zaMesaz.get(i).getNumberPP());
+                Cell cell4_2 = row.createCell(1);
+                row.getCell(1).setCellStyle(style2);
+                cell4_2.setCellValue( stipendia_zaMesaz.get(i).getStatusStudent());
+                if(stipendia_zaMesaz.get(i).getStatusStudent().equals("ц"))
+                    cE++;
+                else
+                    oD++;
+                Cell cell4_3 = row.createCell(2);
+                row.getCell(2).setCellStyle(style2);
+                cell4_3.setCellValue(stipendia_zaMesaz.get(i).getFio());
+                Cell cell4_4 = row.createCell(3);
+                row.getCell(3).setCellStyle(style2);
+                cell4_4.setCellValue(stipendia_zaMesaz.get(i).getSummaR());
+                summ = summ + stipendia_zaMesaz.get(i).getSummaR();
+                Cell cell4_5 = row.createCell(4);
+                row.getCell(4).setCellStyle(style1);
+                cell4_5.setCellValue(stipendia_zaMesaz.get(i).getMounts());
+
+            }
             row = sheet.createRow(i+3);
-            Cell cell4_1 = row.createCell(0);
-            row.getCell(0).setCellStyle(style2);
-            cell4_1.setCellValue(stipendia_zaMesaz.get(i).getNumberPP());
-            Cell cell4_2 = row.createCell(1);
-            row.getCell(1).setCellStyle(style2);
-            cell4_2.setCellValue( stipendia_zaMesaz.get(i).getStatusStudent());
-            if(stipendia_zaMesaz.get(i).getStatusStudent().equals("ц"))
-                cE++;
-            else
-                oD++;
-            Cell cell4_3 = row.createCell(2);
-            row.getCell(2).setCellStyle(style2);
-            cell4_3.setCellValue(stipendia_zaMesaz.get(i).getFio());
-            Cell cell4_4 = row.createCell(3);
-            row.getCell(3).setCellStyle(style2);
-            cell4_4.setCellValue(stipendia_zaMesaz.get(i).getSummaR());
-            summ = summ + stipendia_zaMesaz.get(i).getSummaR();
-            Cell cell4_5 = row.createCell(4);
+            Cell cell5_1 = row.createCell(0);
+            row.getCell(0).setCellStyle(style1);
+            Cell cell5_2 = row.createCell(1);
+            row.getCell(1).setCellStyle(style1);
+            Cell cell5_3 = row.createCell(2);
+            row.getCell(2).setCellStyle(style);
+            cell5_3.setCellValue("Ц = " + cE + " чел.,   О = " + oD + " чел.,   ИТОГО:");
+            Cell cell5_4 = row.createCell(3);
+            row.getCell(3).setCellStyle(style);
+            cell5_4.setCellValue(summ);
+            Cell cell5_5 = row.createCell(4);
             row.getCell(4).setCellStyle(style1);
-            cell4_5.setCellValue(stipendia_zaMesaz.get(i).getMounts());
 
+            row = sheet.createRow(i+4);
+            Cell cell6_1 = row.createCell(0);
+            row.getCell(0).setCellStyle(style3);
+            Cell cell6_2 = row.createCell(1);
+            row.getCell(1).setCellStyle(style3);
+            Cell cell6_3 = row.createCell(2);
+            row.getCell(2).setCellStyle(style3);
+            cell6_3.setCellValue("Ответсвенное лицо _________________");
+            Cell cell6_4 = row.createCell(3);
+            row.getCell(3).setCellStyle(style3);
+            cell6_4.setCellValue("         Подпись");
+            Cell cell6_5 = row.createCell(4);
+            cell6_5.setCellValue("______________");
+            row.getCell(4).setCellStyle(style3);
+
+            row = sheet.createRow(i+6);
+            Cell cell7_4 = row.createCell(3);
+            cell7_4.setCellValue("              Дата");
+            Cell cell8_5 = row.createCell(4);
+            cell8_5.setCellValue("______________");
+
+
+            // Записываем всё в файл
+            FileOutputStream fin = new FileOutputStream(path);
+            book.write(fin);
+            book.close();
+            fin.close();
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        row = sheet.createRow(i+3);
-        Cell cell5_1 = row.createCell(0);
-        row.getCell(0).setCellStyle(style1);
-        Cell cell5_2 = row.createCell(1);
-        row.getCell(1).setCellStyle(style1);
-        Cell cell5_3 = row.createCell(2);
-        row.getCell(2).setCellStyle(style);
-        cell5_3.setCellValue("Ц = " + cE + " чел.,   О = " + oD + " чел.,   ИТОГО:");
-        Cell cell5_4 = row.createCell(3);
-        row.getCell(3).setCellStyle(style);
-        cell5_4.setCellValue(summ);
-        Cell cell5_5 = row.createCell(4);
-        row.getCell(4).setCellStyle(style1);
-
-        row = sheet.createRow(i+4);
-        Cell cell6_1 = row.createCell(0);
-        row.getCell(0).setCellStyle(style3);
-        Cell cell6_2 = row.createCell(1);
-        row.getCell(1).setCellStyle(style3);
-        Cell cell6_3 = row.createCell(2);
-        row.getCell(2).setCellStyle(style3);
-        cell6_3.setCellValue("Ответсвенное лицо _________________");
-        Cell cell6_4 = row.createCell(3);
-        row.getCell(3).setCellStyle(style3);
-        cell6_4.setCellValue("         Подпись");
-        Cell cell6_5 = row.createCell(4);
-        cell6_5.setCellValue("______________");
-        row.getCell(4).setCellStyle(style3);
-
-        row = sheet.createRow(i+6);
-        Cell cell7_4 = row.createCell(3);
-        cell7_4.setCellValue("              Дата");
-        Cell cell8_5 = row.createCell(4);
-        cell8_5.setCellValue("______________");
-
-
-        // Записываем всё в файл
-        FileOutputStream fin = new FileOutputStream(path);
-        book.write(fin);
-        book.close();
-        fin.close();
     }
 
     public void readFromExcel() throws IOException {
@@ -290,6 +295,7 @@ public class Poi_read {
 
             boolean start = workbook.getSheetAt(i).getPhysicalNumberOfRows() > 0;
 
+            rowIterator.next();
             while (start) {
                 start = rowIterator.hasNext();
                 stipendiaZaGods.add(new StipendiaZaGod());
@@ -398,7 +404,5 @@ public class Poi_read {
         }
         return stipendia_zaMesazs;
     }
-
-
 }
 
--- src/main/java/sampleController.java
+++ src/main/java/sampleController.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -10,12 +11,18 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
-public class sampleController {
+import javax.swing.*;
+
+public class sampleController extends Component {
 
     @FXML
     private ResourceBundle resources;
@@ -80,6 +87,8 @@ public class sampleController {
     private TableColumn<Stipendia_zaMesaz, String> mounts;
     @FXML
     private TableView<Stipendia_zaMesaz> stipMesaz;
+    @FXML
+    private Label lab;
 
     String pathToNewFile = null;
     String pathToOpenFile = null;
@@ -90,6 +99,10 @@ public class sampleController {
         ObservableList<String> mounts = FXCollections.observableArrayList("Январь", "Февраль", "Март", "Апрель",
                                                                                 "Май", "Июнь", "Июль", "Август",
                                                                                 "Сентябрь", "Октябрь", "Ноябрь", "Декабрь");
+        selectTimes.setItems(mounts);
+        lab.setVisible(false);
+        selectTimes.setVisible(false);
+
         addOtchot.setOnAction(e->{
 
             switch (selectTyprOtchot.getValue()) {
@@ -103,17 +116,34 @@ public class sampleController {
 
                         Poi_read poi_read = new Poi_read(null);
                         ArrayList <Stipendia_zaMesaz> stipendia_zaMesaz = poi_read.readToStipZaGod(pathToExelFile);
-                        poi_read.writeIntoExcelStipendiaYear(pathToNewFile, stipendia_zaMesaz, "2010");
+                        if (selectTimes.getValue() == null) {
+                            JFrame topFrame = (JFrame)SwingUtilities.getWindowAncestor(this);
+                            JOptionPane.showMessageDialog(topFrame, "Год не выбран!!!");
+                            return;
+                        }
+                        if (poi_read.writeIntoExcelStipendiaYear(pathToNewFile, stipendia_zaMesaz, selectTimes.getValue())) {
+                            System.out.println("Вы знаете что такое успех?..");
+                            JFrame topFrame = (JFrame)SwingUtilities.getWindowAncestor(this);
+                            JOptionPane.showMessageDialog(topFrame, "Файл успешно создан!");
+                        } else {
+                            System.out.println("Вы уволены на 60%?..");
+                            JFrame topFrame = (JFrame)SwingUtilities.getWindowAncestor(this);
+                            JOptionPane.showMessageDialog(topFrame, "Файл успешно не создан!");
+                        }
                     } catch (IOException ex) {
                         ex.printStackTrace();
                     }
                     break;
                 case "Месячный отчет по фонду заработной платы" :
                     break;
+                default:
+                    JFrame topFrame = (JFrame)SwingUtilities.getWindowAncestor(this);
+                    JOptionPane.showMessageDialog(topFrame, "не выбран тип отчета!");
+                    break;
             }
 
         });
-        selectTimes.setItems(mounts);
+
 
         otchot1.setVisible(false);
         otchot2.setVisible(false);
@@ -121,7 +151,7 @@ public class sampleController {
         otchot4.setVisible(false);
         tableEmpty.setVisible(true);
 
-        ObservableList<String> typeOtchot = FXCollections.observableArrayList("?", "Годовая выплата стипендии", "Месячный отчет по фонду заработной платы");
+        ObservableList<String> typeOtchot = FXCollections.observableArrayList("Выплата стипендии за месяц", "Годовая выплата стипендии");
         selectTyprOtchot.setItems(typeOtchot);
 
         newFile.setOnAction(e -> {
@@ -138,13 +168,17 @@ public class sampleController {
         });
 
         selectTyprOtchot.setOnAction(e -> {
+            lab.setVisible(true);
+            selectTimes.setVisible(true);
             switch (selectTyprOtchot.getValue()) {
-                case "?" :
+                case "Выплата стипендии за месяц" :
                     otchot1.setVisible(true);
                     otchot2.setVisible(false);
                     otchot3.setVisible(false);
                     otchot4.setVisible(false);
                     tableEmpty.setVisible(false);
+                    selectTimes.setItems(mounts);
+
                     break;
                 case "Годовая выплата стипендии" :
                     otchot1.setVisible(false);
@@ -152,6 +186,13 @@ public class sampleController {
                     otchot3.setVisible(true);
                     otchot4.setVisible(false);
                     tableEmpty.setVisible(false);
+
+                    lab.setText("Год");
+                    ObservableList<String> year = FXCollections.observableArrayList("1980");
+                    for (int i = 1981; i < 2021; i++) {
+                        year.add(String.valueOf(i));
+                    }
+                    selectTimes.setItems(year);
 
                     try {
                         ObservableList<Stipendia_zaMesaz> Stipendia_zaMesazData = FXCollections.observableArrayList();
