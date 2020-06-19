a12d34049d479a3f479328cef69ff50da6c3aa27
Ersis404 <58598953+Ersis404@users.noreply.github.com>
2020-06-19 18:55:43 +0300

    prodactionCreate

--- src/main/java/Poi/Poi_read.java
+++ src/main/java/Poi/Poi_read.java
@@ -11,40 +11,6 @@ import java.util.Iterator;
 public class Poi_read {
     String path;
 
-    public Poi_read(String path) throws IOException {
-        this.path = path;
-    }
-
-    private HSSFFont createCellFont(HSSFWorkbook book) {
-
-        HSSFFont font = book.createFont();
-        font.setFontHeightInPoints((short) 12);
-        font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-        font.setFontName("Times New Roman");
-        return (font);
-    }
-
-
-    private HSSFCellStyle createCellStyle(HSSFWorkbook book) {
-        BorderStyle thin = BorderStyle.THIN;
-        short black = IndexedColors.BLACK.getIndex();
-
-        HSSFCellStyle style = book.createCellStyle();
-
-        style.setWrapText(true);
-        style.setAlignment(CellStyle.ALIGN_CENTER);
-        style.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
-
-        style.setTopBorderColor(black);
-        style.setRightBorderColor(black);
-        style.setBottomBorderColor(black);
-        style.setLeftBorderColor(black);
-
-        return style;
-    }
-
-    //String pathCreate, ArrayList<Stipendia_zaMesaz> listCreate
-    @SuppressWarnings("deprecation")
     public boolean writeIntoExcelStipendiaYear(String path, ArrayList<Stipendia_zaMesaz> stipendia_zaMesaz, String year) throws FileNotFoundException, IOException {
 
         try {
@@ -54,22 +20,14 @@ public class Poi_read {
             HSSFFont font = book.createFont();
 
             HSSFCellStyle style = book.createCellStyle();
-            HSSFCellStyle style1 = book.createCellStyle();
+
             HSSFCellStyle style2 = book.createCellStyle();
             HSSFCellStyle style3 = book.createCellStyle();
 
 
-            style.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
             style.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
             style.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
             style.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
-            style1.setAlignment(style.ALIGN_CENTER);
-            style1.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
-            style1.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
-            style1.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
-            style1.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
-            style1.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
-            style2.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
             style2.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
             style2.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
             style2.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
@@ -86,8 +44,8 @@ public class Poi_read {
             style.setAlignment(style.ALIGN_CENTER);
 
             HSSFRow row = sheet.createRow((short) 0);
-            style1.setWrapText(true);
-            row.setRowStyle(style1);
+            style2.setWrapText(true);
+            row.setRowStyle(style2);
 
             sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 4));
             sheet.addMergedRegion(new CellRangeAddress(1, 1, 0, 4));
@@ -143,6 +101,7 @@ public class Poi_read {
             int oD = 0;
             int cE = 0;
             int i;
+
             for (i = 0; i < stipendia_zaMesaz.size(); i++) {
                 row = sheet.createRow(i + 3);
                 Cell cell4_1 = row.createCell(0);
@@ -163,15 +122,15 @@ public class Poi_read {
                 cell4_4.setCellValue(stipendia_zaMesaz.get(i).getSummaR());
                 summ = summ + stipendia_zaMesaz.get(i).getSummaR();
                 Cell cell4_5 = row.createCell(4);
-                row.getCell(4).setCellStyle(style1);
+                row.getCell(4).setCellStyle(style2);
                 cell4_5.setCellValue(stipendia_zaMesaz.get(i).getMounts());
 
             }
             row = sheet.createRow(i + 3);
             Cell cell5_1 = row.createCell(0);
-            row.getCell(0).setCellStyle(style1);
+            row.getCell(0).setCellStyle(style2);
             Cell cell5_2 = row.createCell(1);
-            row.getCell(1).setCellStyle(style1);
+            row.getCell(1).setCellStyle(style2);
             Cell cell5_3 = row.createCell(2);
             row.getCell(2).setCellStyle(style);
             cell5_3.setCellValue("Ц = " + cE + " чел.,   О = " + oD + " чел.,   ИТОГО:");
@@ -179,7 +138,7 @@ public class Poi_read {
             row.getCell(3).setCellStyle(style);
             cell5_4.setCellValue(summ);
             Cell cell5_5 = row.createCell(4);
-            row.getCell(4).setCellStyle(style1);
+            row.getCell(4).setCellStyle(style2);
 
             row = sheet.createRow(i + 4);
             Cell cell6_1 = row.createCell(0);
@@ -203,7 +162,7 @@ public class Poi_read {
             cell8_5.setCellValue("______________");
 
 
-            // Записываем всё в файл
+
             FileOutputStream fin = new FileOutputStream(path);
             book.write(fin);
             book.close();
@@ -217,28 +176,19 @@ public class Poi_read {
     public boolean writeIntoExcelStipendiaMount(String path, ArrayList<godovaiaStipendia> list, String mount) {
 
         try {
+            System.out.println("5");
             HSSFWorkbook book = new HSSFWorkbook();
             HSSFSheet sheet = book.createSheet("Стипендия_за_месяц");
 
             HSSFFont font = book.createFont();
 
             HSSFCellStyle style = book.createCellStyle();
-            HSSFCellStyle style1 = book.createCellStyle();
             HSSFCellStyle style2 = book.createCellStyle();
             HSSFCellStyle style3 = book.createCellStyle();
 
-
-//            style.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
             style.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
             style.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
             style.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
-            style1.setAlignment(style.ALIGN_CENTER);
-            style1.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
-            style1.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
-            style1.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
-            style1.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
-            style1.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
-            style2.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
             style2.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
             style2.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);
             style2.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);
@@ -247,7 +197,7 @@ public class Poi_read {
             style3.setBottomBorderColor(HSSFCellStyle.BORDER_MEDIUM);
             style3.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);
 
-            // и применяем к этому стилю жирный шрифт
+
             style.setFont(font);
             font.setFontHeightInPoints((short) 12);
             font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
@@ -255,8 +205,8 @@ public class Poi_read {
             style.setAlignment(style.ALIGN_CENTER);
 
             HSSFRow row = sheet.createRow((short) 0);
-            style1.setWrapText(true);
-            row.setRowStyle(style1);
+            style2.setWrapText(true);
+            row.setRowStyle(style2);
 
             sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 3));
             sheet.addMergedRegion(new CellRangeAddress(1, 1, 0, 3));
@@ -266,26 +216,25 @@ public class Poi_read {
             Cell cell1_1 = row.createCell(1);
             Cell cell1_2 = row.createCell(2);
             Cell cell1_3 = row.createCell(3);
-//            Cell cell1_4 = row.createCell(4);
+
             row.getCell(0).setCellStyle(style);
             row.getCell(1).setCellStyle(style);
             row.getCell(2).setCellStyle(style);
             row.getCell(3).setCellStyle(style);
-//            row.getCell(4).setCellStyle(style);
+
             cell1.setCellValue("С Т И П Е Н Д И Я");
             row = sheet.createRow(1);
             Cell cell2 = row.createCell(0);
             Cell cell2_1 = row.createCell(1);
             Cell cell2_2 = row.createCell(2);
             Cell cell2_3 = row.createCell(3);
-//            Cell cell2_4 = row.createCell(4);
+
             row.getCell(0).setCellStyle(style);
             row.getCell(1).setCellStyle(style);
             row.getCell(2).setCellStyle(style);
             row.getCell(3).setCellStyle(style);
-            row.getCell(4).setCellStyle(style);
-//            row.getCell(0).setCellStyle(style);
-            cell2.setCellValue(" За " + mount);
+
+            cell2.setCellValue(" За " + mount.toLowerCase() + " месяц");
             row = sheet.createRow(2);
             Cell cell3_1 = row.createCell(0);
             row.getCell(0).setCellStyle(style);
@@ -303,15 +252,7 @@ public class Poi_read {
             row.getCell(3).setCellStyle(style);
             cell3_4.setCellValue("Сумма, руб");
             sheet.autoSizeColumn(3);
-//            Cell cell3_5 = row.createCell(4);
-//            row.getCell(4).setCellStyle(style);
-//            cell3_5.setCellValue("Примечание");
-//            sheet.autoSizeColumn(4);
 
-//            System.out.println(el.getNumberPP());
-//            System.out.println(el.getFIO());
-//            System.out.println(el.getStatusSt());
-//            System.out.println(el.getSumma());
 
             double summ = 0;
             int oD = 0;
@@ -336,26 +277,25 @@ public class Poi_read {
                 row.getCell(3).setCellStyle(style2);
                 cell4_4.setCellValue(list.get(i).getSumma());
                 summ = summ + list.get(i).getSumma();
-//                Cell cell4_5 = row.createCell(4);
-//                row.getCell(4).setCellStyle(style1);
-//                cell4_5.setCellValue(stipendia_zaMesaz.get(i).getMounts());
+
 
             }
-            sheet.addMergedRegion(new CellRangeAddress(i+3, i+3, 0, 2));
+            sheet.addMergedRegion(new CellRangeAddress(i + 3, i + 3, 0, 2));
             row = sheet.createRow(i + 3);
             Cell cell5_1 = row.createCell(0);
-//            row.getCell(0).setCellStyle(style1);
+            cell5_1.setCellValue("Ц = " + cE + " чел.,   О = " + oD + " чел., Всего = " + (cE + oD) + " чел.. ИТОГ:");
+            row.getCell(0).setCellStyle(style);
             Cell cell5_2 = row.createCell(1);
-            row.getCell(1).setCellStyle(style1);
+            row.getCell(1).setCellStyle(style);
             Cell cell5_3 = row.createCell(2);
             row.getCell(2).setCellStyle(style);
-            cell5_1.setCellValue("Ц = " + cE + " чел.,   О = " + oD + " чел., Всего = " + (cE+oD) + " чел.. ИТОГ:");
-            Cell cell5_4 = row.createCell(3);
-            row.getCell(0).setCellStyle(style);
-            cell5_4.setCellValue(summ);
-//            Cell cell5_5 = row.createCell(4);
-//            row.getCell(4).setCellStyle(style1);
 
+            Cell cell5_4 = row.createCell(3);
+
+            cell5_4.setCellValue(summ);
+
+            row.getCell(3).setCellStyle(style);
+            sheet.addMergedRegion(new CellRangeAddress(i + 4, i + 4, 0, 3));
             row = sheet.createRow(i + 4);
             Cell cell6_1 = row.createCell(0);
             row.getCell(0).setCellStyle(style3);
@@ -363,24 +303,20 @@ public class Poi_read {
             row.getCell(1).setCellStyle(style3);
             Cell cell6_3 = row.createCell(2);
             row.getCell(2).setCellStyle(style3);
-            cell6_3.setCellValue("Ответсвенное лицо _________________");
+            cell6_1.setCellValue("Ответсвенное лицо _________________         Подпись ______________");
             Cell cell6_4 = row.createCell(3);
             row.getCell(3).setCellStyle(style3);
-            cell6_4.setCellValue("         Подпись");
+
             Cell cell6_5 = row.createCell(4);
-            cell6_5.setCellValue("______________");
-//            row.getCell(4).setCellStyle(style3);
 
+
+            sheet.addMergedRegion(new CellRangeAddress(i + 6, i + 6, 0, 3));
             row = sheet.createRow(i + 6);
-            Cell cell7_4 = row.createCell(3);
-            cell7_4.setCellValue("              Дата");
+            Cell cell7_1 = row.createCell(0);
+            cell7_1.setCellValue("              Дата ______________");
             Cell cell8_5 = row.createCell(4);
-            cell8_5.setCellValue("______________");
 
-
-            // Записываем всё в файл
             FileOutputStream fin = new FileOutputStream(path);
-            System.out.println("11111111111111111111111111111111111111");
             book.write(fin);
             book.close();
             fin.close();
@@ -391,64 +327,6 @@ public class Poi_read {
 
     }
 
-
-    public void readFromExcel() throws IOException {
-
-//        String s1;
-//        HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream("D:\\ter.xls"));
-//        HSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
-//        HSSFRow row ;
-//        for (int i = 8; i < 22; i++){
-//            row = myExcelSheet.getRow(i);
-//            s1 = row.getCell(1).getStringCellValue();
-//            System.out.println(s1 +"|"+ s2 +"|"+s3 +"|"+s4 +"|"+s5 +"|"+s6 +"|"+s7 +"|"+s8 +"|"+s9 +"|"+s10);
-//        }
-//        myExcelBook.close();
-//        HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream("D:\\ter.xls"));
-//        HSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
-
-        FileInputStream fis = new FileInputStream(new File(path));
-
-//            XSSFWorkbook workbook = new XSSFWorkbook(fis);
-//            XSSFSheet spreadsheet = workbook.getSheetAt(0);
-//            XSSFRow row;
-        HSSFRow row;
-        HSSFWorkbook workbook = new HSSFWorkbook(fis);
-        HSSFSheet spreadsheet = null;
-        System.out.println(workbook.getNumberOfSheets());
-        for (int i = 0; workbook.getNumberOfSheets() > i; i++) {
-            spreadsheet = workbook.getSheetAt(i);
-            Iterator<Row> rowIterator = spreadsheet.iterator();
-
-            while (rowIterator.hasNext()) {
-//                row = (XSSFRow) rowIterator.next();
-                row = (HSSFRow) rowIterator.next();
-                Iterator<Cell> cellIterator = row.cellIterator();
-
-                while (cellIterator.hasNext()) {
-                    Cell cell = cellIterator.next();
-
-                    switch (cell.getCellType()) {
-                        case Cell.CELL_TYPE_NUMERIC:
-                            System.out.print(cell.getNumericCellValue() + " \t\t ");
-                            break;
-
-                        case Cell.CELL_TYPE_STRING:
-                            System.out.print(
-                                    cell.getStringCellValue() + " \t\t ");
-                            break;
-                        case Cell.CELL_TYPE_FORMULA:
-                            System.out.print(cell.getNumericCellValue() + " \t\t ");
-                            break;
-                    }
-                }
-                System.out.println();
-            }
-            fis.close();
-            System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-        }
-
-    }
 
     public ArrayList<Stipendia_zaMesaz> readToStipZaGod(String path) throws IOException {
 
@@ -564,14 +442,6 @@ public class Poi_read {
                         (el.getManyForSentabr() != 0 ? "сентябрь\n" : "") + (el.getManyForOkt() != 0 ? "октябрь\n" : "") + (el.getManyForNoabr() != 0 ? "ноябрь\n" : "") + (el.getManyForDekabr() != 0 ? "декабрь" : ""));
             });
 
-//            stipendia_zaMesazs.stream().forEach(el -> {
-//                System.out.print( el.getFio() + "\t");
-//                System.out.print( el.getNumberPP() + "\t");
-//                System.out.print( el.getStatusStudent() + "\t");
-//                System.out.print( el.getSummaR() + "\t");
-//                System.out.print( el.getMounts() + "\t");
-//            });
-//            System.out.println("------------------------------------------------------------------------------------------------------------------------------");
         }
         return stipendia_zaMesazs;
     }
